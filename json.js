a026d5198e58740953e98e1b662e1875f09089f9
thanhson <kimsonbui303@gmail.com>
2019-06-21 10:44:12 +0700

    write json file

---
+++ json.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+
+
+function readJsonFile() {
+    let dataFile = fs.readFileSync('./sample/sample.json');
+
+    let data = JSON.parse(dataFile);   
+    
+    return data;
+
+}
+
+
+function writeJsonFile() {
+    let data = {
+        name: 'sonbkt',
+        address: '1K7'
+    }
+    fs.writeFileSync('./output/output.json', JSON.stringify(data));
+
+    console.log('Write file successfully');
+}
+
+writeJsonFile();
